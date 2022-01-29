5dbff037358c76e361726f44b72eaa770515a979
jonahBerlyne <jonshaber@gmail.com>
2022-01-29 05:04:18 -0500

    505am 1/29/22 added form

--- src/Components/QR.js
+++ src/Components/QR.js
@@ -4,13 +4,42 @@ var QRCode = require('qrcode.react');
 export default function QR() {
 
  const [url, setUrl] = useState('');
+ console.log(`url is: ${url}`);
+ const [refresh, setRefresh] = useState(false);
+
+ const [qrs, setQrs] = useState([]);
+
+ const handleChange = e => {
+  setUrl(e.target.value);
+ }
+
+ const [empty, setEmpty] = useState(true);
+
+ const onSubmit = e => {
+  e.preventDefault();
+  if (url == '') {
+   alert("Please enter a url to generate a QR Code.");
+   return;
+  }
+  setQrs(qrs => [...qrs, url]);
+  setEmpty(false);
+  setUrl('');
+ }
+
  return (
   <div>
    <h4>Enter a website:</h4>
-   <input placeholder='www.website.com'/>
+   <form onSubmit={onSubmit}>
+    <input value={url} onChange={handleChange} placeholder='www.website.com'/>
+    <br/>
+    <br/>
+    <button type="submit">Generate QR Code</button>
+   </form>
    <br/>
    <br/>
-   <QRCode value="http://nbc.com/" />
+   <br/>
+   <br/>
+   {!empty && <QRCode value={url} />}
   </div>
  );
 }
