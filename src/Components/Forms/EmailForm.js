f1120d06901250f0689efd21f48044dcce65eff8
jonahBerlyne <jonshaber@gmail.com>
2022-01-31 03:27:59 -0500

    328amm 1/31/22 added inputs to email form

--- src/Components/Forms/EmailForm.js
+++ src/Components/Forms/EmailForm.js
@@ -3,7 +3,14 @@ import React, { useState, useEffect } from 'react';
 export default function EmailForm() {
  return (
   <div>
-    Email Form
+   <h4>Enter your e-mail:</h4>
+    <input type="email" placeholder="Enter your e-mail address"/>
+    <br/>
+    <br/>
+    <input type="text" placeholder='Enter e-mail subject'/>
+    <br/>
+    <br/>
+    <textarea rows="10" cols="40" placeholder='Enter your message here'/>
   </div>
  );
 }
