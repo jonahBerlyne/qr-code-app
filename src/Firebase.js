fd86c98d4cbf786fab992b887b548f56bee8e462
jonahBerlyne <jonshaber@gmail.com>
2022-02-06 12:14:52 -0500

    added firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtVk6VHuKv09QlPZMtdO7qgERbvJIuiVY",
+  authDomain: "qr-code-app-ff5c2.firebaseapp.com",
+  projectId: "qr-code-app-ff5c2",
+  storageBucket: "qr-code-app-ff5c2.appspot.com",
+  messagingSenderId: "127543328152",
+  appId: "1:127543328152:web:db35dc3ca028d7d9a97fed"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
