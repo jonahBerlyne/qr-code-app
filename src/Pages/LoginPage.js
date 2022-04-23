c9273bb7551debea7089c0fdfbea86354d03ee3b
jonahBerlyne <jonshaber@gmail.com>
2022-04-22 20:44:02 -0400

    created login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,3 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export default function LoginPage () {}
+export default function LoginPage () {
+
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+ const auth = getAuth();
+
+ const login = async () => {
+  try {
+   await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+   alert(`Error: ${err}`);
+  }
+ }
+
+ return (
+  <div className="field">
+   <h2>Login:</h2>
+   <input 
+    type="email" 
+    className="auth-input" placeholder='Email' 
+    value={email} 
+    onChange={(e) => {setEmail(e.target.value)}}
+   />
+   <input 
+    type="password" 
+    className="auth-input" placeholder='Password' 
+    value={password} 
+    onChange={(e) => {setPassword(e.target.value)}}
+   />
+   <button className="auth-btn" onClick={login}>Login</button>
+   <hr/>
+   <Link to="/register">Click Here to Register</Link>
+  </div>
+ );
+}
