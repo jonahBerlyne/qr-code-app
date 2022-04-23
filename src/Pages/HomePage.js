961f14f30d034caeead864dfdfc472f46f239f43
jonahBerlyne <jonshaber@gmail.com>
2022-04-22 21:05:36 -0400

    added logout fn to homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,25 @@
 import QR from "../Components/QR";
 import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function HomePage() {
+
+  const auth = getAuth();
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      alert(`Signout error: ${err}`);
+    }
+  }
+  
   return (
     <div className="App">
       <h1>QR Code App</h1>
       <QR/>
       <Link to="/codes">Your QR Codes</Link>
+      <button onClick={logOut}>Sign Out</button>
     </div>
   );
 }
