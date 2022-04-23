ebf9167c412097e3f928554f6e9c606c64987976
jonahBerlyne <jonshaber@gmail.com>
2022-04-22 20:55:43 -0400

    added app route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Codes from "./Components/Codes/Codes";
-import Auth from "./Firebase/Auth";
 import HomePage from "./Pages/HomePage";
 import SignUpPage from "./Pages/SignUpPage";
 import LoginPage from "./Pages/LoginPage";
+import AppRoute from "./Routes/AppRoute";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<SignUpPage/>}/>
-        <Route path="/codes" element={<Codes/>}/>
+        <Route path="/home" element={<AppRoute><HomePage/></AppRoute>}/>
+        <Route path="/codes" element={<AppRoute><Codes/></AppRoute>}/>
       </Routes>
     </Router>
   );
