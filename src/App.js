ddf0fc941774e8d815247f010c5478c82dcabc16
jonahBerlyne <jonshaber@gmail.com>
2022-04-22 20:57:36 -0400

    added auth route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import HomePage from "./Pages/HomePage";
 import SignUpPage from "./Pages/SignUpPage";
 import LoginPage from "./Pages/LoginPage";
 import AppRoute from "./Routes/AppRoute";
+import AuthRoute from "./Routes/AuthRoute";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<SignUpPage/>}/>
+        <Route path="/login" element={<AuthRoute><LoginPage/></AuthRoute>}/>
+        <Route path="/register" element={<AuthRoute><SignUpPage/></AuthRoute>}/>
         <Route path="/home" element={<AppRoute><HomePage/></AppRoute>}/>
         <Route path="/codes" element={<AppRoute><Codes/></AppRoute>}/>
       </Routes>
