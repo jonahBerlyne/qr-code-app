e36fe5ef61564890f2fca47da58952c65a98c547
jonahBerlyne <jonshaber@gmail.com>
2022-04-22 20:37:00 -0400

    added signup and login pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Codes from "./Components/Codes/Codes";
 import Auth from "./Firebase/Auth";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Auth/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<SignUpPage/>}/>
         <Route path="/codes" element={<Codes/>}/>
       </Routes>
     </Router>
