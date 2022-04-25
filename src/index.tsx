b61556a8e0004fe5359196c1a3b65dd49b1bedee
jonahBerlyne <jonshaber@gmail.com>
2022-04-25 01:47:33 -0400

    reconfigured index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from "./Redux/Store";
 import "./index.css";
@@ -11,11 +11,11 @@ import { Provider } from "react-redux";
 //   );
 // }
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  // {/* </React.StrictMode>, */}
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
