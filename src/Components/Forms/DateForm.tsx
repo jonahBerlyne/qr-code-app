af6b90772fe580c818b0b708f8e709c69b7e6001
jonahBerlyne <jonshaber@gmail.com>
2022-04-28 17:04:59 -0400

    added types to date form

--- src/Components/Forms/DateForm.tsx
+++ src/Components/Forms/DateForm.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FormInterface } from "../QR";
 
-export default function DateForm({ values, handleChange }) {
+export default function DateForm({ values, handleChange }: FormInterface) {
 
- var today = new Date();
- var dd = today.getDate();
- var mm = today.getMonth() + 1; // January is 0
- var yyyy = today.getFullYear();
+ const [currentDate, setCurrentDate] = useState<string>("");
 
- if (dd < 10) {
-    dd = '0' + dd;
- }
+ useEffect(() => {
+   const date = new Date();
+   let dd: string | number = date.getDate();
+   let mm: string | number = date.getMonth() + 1; // January is 0
+   const yyyy: number = date.getFullYear();
+  
+   if (dd < 10) dd = '0' + dd;
+   if (mm < 10) mm = '0' + mm;
+   setCurrentDate(`${yyyy}-${mm}-${dd}`);
 
- if (mm < 10) {
-    mm = '0' + mm;
- } 
-
- today = yyyy + '-' + mm + '-' + dd;
+   return () => {
+      setCurrentDate("");
+   }
+ }, []);
 
  return (
   <div>
    <h4>Set an event:</h4>
    <h4>From:</h4>
-   <input type="date" min={today} name="fromDate" value={values.fromDate} onChange={handleChange} required/>
+   <input type="date" min={currentDate} name="fromDate" value={values.fromDate} onChange={handleChange} required/>
    <br/>
    <br/>
    <h4>To:</h4>
-   <input type="date" min={today} name="toDate" value={values.toDate} onChange={handleChange} required/>
+   <input type="date" min={currentDate} name="toDate" value={values.toDate} onChange={handleChange} required/>
    <br/>
    <br/>
    <h4>Event:</h4>
@@ -33,7 +36,7 @@ export default function DateForm({ values, handleChange }) {
    <h4>Location:</h4>
    <input type="text" name="location" value={values.location} onChange={handleChange} placeholder='Enter event location'/>
    <h4>Details:</h4>
-   <textarea name="details" value={values.details} onChange={handleChange} rows="10" cols="40" placeholder='Enter event details'/>
+   <textarea name="details" value={values.details} onChange={handleChange} rows={10} cols={40} placeholder='Enter event details'/>
   </div>
  );
 }
