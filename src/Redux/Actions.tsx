843cc6c164420e0704b0d1f7daea73723c443b23
jonahBerlyne <jonshaber@gmail.com>
2022-04-28 17:50:41 -0400

    added types to actions

--- src/Redux/Actions.tsx
+++ src/Redux/Actions.tsx
@@ -1,6 +1,6 @@
 import * as actions from "./ActionTypes";
 
-export const addContactCode = (id, first, last, card) => ({
+export const addContactCode = (id: string | undefined, first: string, last: string, card: string) => ({
  type: actions.ADD_CONTACT_CODE,
  payload: {
   id,
@@ -10,7 +10,7 @@ export const addContactCode = (id, first, last, card) => ({
  }
 });
 
-export const addDateCode = (id, from, to, event, location, details) => ({
+export const addDateCode = (id: string | undefined, from: string, to: string, event: string, location: string, details: string) => ({
  type: actions.ADD_DATE_CODE,
  payload: {
   id,
@@ -22,7 +22,7 @@ export const addDateCode = (id, from, to, event, location, details) => ({
  }
 });
 
-export const addEmailCode = (id, to, subj, msg, email) => ({
+export const addEmailCode = (id: string | undefined, to: string, subj: string, msg: string, email: string) => ({
  type: actions.ADD_EMAIL_CODE,
  payload: {
   id,
@@ -33,7 +33,7 @@ export const addEmailCode = (id, to, subj, msg, email) => ({
  }
 });
 
-export const addImgCode = (id, img) => ({
+export const addImgCode = (id: string | undefined, img: string) => ({
  type: actions.ADD_IMG_CODE,
  payload: {
   id,
@@ -41,7 +41,7 @@ export const addImgCode = (id, img) => ({
  }
 });
 
-export const addTextCode = (id, text) => ({
+export const addTextCode = (id: string | undefined, text: string) => ({
  type: actions.ADD_TEXT_CODE,
  payload: {
   id,
@@ -49,7 +49,7 @@ export const addTextCode = (id, text) => ({
  }
 });
 
-export const addUrlCode = (id, url) => ({
+export const addUrlCode = (id: string | undefined, url: string) => ({
  type: actions.ADD_URL_CODE,
  payload: {
   id,
@@ -57,7 +57,7 @@ export const addUrlCode = (id, url) => ({
  }
 });
 
-export const deleteQRCode = id => ({
+export const deleteQRCode = (id: string | undefined) => ({
  type: actions.DELETE_QR_CODE,
  payload: {
   id
