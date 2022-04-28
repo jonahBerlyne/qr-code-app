b6d9dc49eb38bb2b9aed521b6d0763e908795112
jonahBerlyne <jonshaber@gmail.com>
2022-04-28 17:52:42 -0400

    fixed rendering bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import store from "./Redux/Store";
-import "./index.css";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
